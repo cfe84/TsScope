925092d62d555e2c976c8d3597e52b3958cf6714
Charles Feval <fevalc>
2025-03-10 09:05:10 -0700

    Namespace filtering not working.

--- ScopeParser/Backend/TypeScriptSnippets/fieldList.ts
+++ ScopeParser/Backend/TypeScriptSnippets/fieldList.ts
@@ -1,9 +1,11 @@
 {
     fieldFilter: (field: QualifiedName) => [__fields__]
+        // TODO: Fix for namespace 
         .includes(field.name) || [__fields__].includes(`${field.namespace}.${field.name}`),
     missingFields: (fields: QualifiedName[]) => {
         const fieldNames = fields.map((field) => field.name);
         const qualifiedNames = fields.map((field) => field.namespace + "." + field.name);
+        // TODO: Fix for namespace 
         const result = [__fields__].filter((field) => !(qualifiedNames.includes(field) || fieldNames.includes(field)));
         return { result, position: "__position__" };
     }
